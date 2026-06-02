5b49b3c34d1db34d4c47993e6b09f3ff432e3500
Kamilla-Parhomenko <parhomenkokamilla@gmail.com>
2026-06-02 22:08:10 +0400

    vid 5

---
+++ utils/include.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const footerComponent = "footer-component.html";
+    const headerComponent = "header-component.html";
+
+    const footer = document.querySelectorAll("#footer");
+    const header = document.querySelectorAll("#header");
+});
