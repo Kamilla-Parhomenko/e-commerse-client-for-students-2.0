f06bdead15446a5ad3c17ebe13e2e4d936e1651f
Kamilla-Parhomenko <parhomenkokamilla@gmail.com>
2026-06-15 15:43:18 +0400

    vid 6-7

--- utils/include.js
+++ utils/include.js
@@ -1,7 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const footerComponent = "footer-component.html";
-    const headerComponent = "header-component.html";
+document.addEventListener('DOMContentLoaded', async () => {
 
-    const footer = document.querySelectorAll("#footer");
-    const header = document.querySelectorAll("#header");
+    const footers = document.querySelectorAll('#footer');
+    const headers = document.querySelectorAll('#header');
+
+    for(let footer of footers) {
+        const src = footer.getAttribute('src');
+
+        const resp = await fetch(src);
+        const html = await resp.text();
+        console.log(html);
+
+        footer.outerHTML = html;
+    }
+
+    for(let header of headers) {
+        const src = header.getAttribute('src');
+
+        const resp = await fetch(src);
+        const html = await resp.text();
+
+        header.outerHTML = html;
+    }
+
+    console.log(footers);
 });
